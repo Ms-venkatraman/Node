323e1d81eff648dcbf0328f126439f06319a54c7
Ms-venkatraman <m.venkat9626@gmail.com>
2025-11-05 08:50:45 +0530

    os command update

---
+++ Task/Day2/os.js
@@ -0,0 +1,8 @@
+import os from 'os';
+console.log("💻 OS Type:", os.type());
+// console.log("🧱 Architecture:", os.arch());
+// console.log("🧠 Total Memory:", os.totalmem());
+// console.log("📉 Free Memory:", os.freemem());
+// console.log("👨‍💻 User Info:", os.userInfo());
+// console.log("⏱ System Uptime:", os.uptime(), "seconds");
+// console.log("💪 CPU Cores:", os.cpus().length);
