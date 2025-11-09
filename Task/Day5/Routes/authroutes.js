e07ae8a8ef1627b8993c455e31ac8d957ee919c6
Ms-venkatraman <m.venkat9626@gmail.com>
2025-11-09 10:20:12 +0530

    delete day5

--- Task/Day5/Routes/authroutes.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { authController } from '../Controller/authcontroller.js';
-const Router=express.Router();
-
-Router.post('/data',authController);
-export default Router;
