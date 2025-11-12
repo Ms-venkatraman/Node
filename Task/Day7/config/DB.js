6b94ba7007d78d261a2dcbf7c292586a91d13273
Ms-venkatraman <m.venkat9626@gmail.com>
2025-11-12 10:15:50 +0530

    update mongo db, task

---
+++ Task/Day7/config/DB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+const connection= async ()=>{
+    try {
+        const data=await mongoose.connect(process.env.MONGO_URI);
+        console.log(`successfully DB connected ${data.connection.host}`);
+        
+    } catch (error) {
+        console.log("DB connection Error :",error)
+    }
+}
+export default connection
